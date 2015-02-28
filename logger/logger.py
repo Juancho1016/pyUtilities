726bf90434413f5633680af59462af35ddb06e7e
Juan Jose Escobar <jjescof@gmail.com>
2015-02-28 13:40:48 -0500

    Importing layout and some files, adding rules to gitignore

---
+++ logger/logger.py
@@ -0,0 +1,28 @@
+import logging
+print 'Testing utilities module'
+
+print 'Folder'
+# create logger with 'spam_application'
+logger = logging.getLogger('spam_application')
+logger.setLevel(logging.DEBUG)
+# create file handler which logs even debug messages
+fh = logging.FileHandler('spam.log')
+fh.setLevel(logging.DEBUG)
+# create console handler with a higher log level
+ch = logging.StreamHandler()
+ch.setLevel(logging.ERROR)
+# create formatter and add it to the handlers
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+ch.setFormatter(formatter)
+# add the handlers to the logger
+logger.addHandler(fh)
+logger.addHandler(ch)
+
+logger.info('Aqui estoy')
+logger.debug('debug message')
+logger.info('info message')
+logger.warn('warn message')
+logger.error('error message')
+logger.critical('critical message')
+
