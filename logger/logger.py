8038e78837cb5f8952eaf2c1a285753ac1c28294
Juan Jose Escobar <jjescof@gmail.com>
2015-03-03 11:24:40 -0500

    Logger class implemented

--- logger/logger.py
+++ logger/logger.py
@@ -1,28 +1,74 @@
 import logging
-print 'Testing utilities module'
+import sys
+import threading
 
-print 'Folder'
-# create logger with 'spam_application'
-logger = logging.getLogger('spam_application')
-logger.setLevel(logging.DEBUG)
-# create file handler which logs even debug messages
-fh = logging.FileHandler('spam.log')
-fh.setLevel(logging.DEBUG)
-# create console handler with a higher log level
-ch = logging.StreamHandler()
-ch.setLevel(logging.ERROR)
-# create formatter and add it to the handlers
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-fh.setFormatter(formatter)
-ch.setFormatter(formatter)
-# add the handlers to the logger
-logger.addHandler(fh)
-logger.addHandler(ch)
-
-logger.info('Aqui estoy')
-logger.debug('debug message')
-logger.info('info message')
-logger.warn('warn message')
-logger.error('error message')
-logger.critical('critical message')
-
+_formatter = '%(asctime)s - %(levelname)s:: %(caller)s:: %(message)s'
+_dateformatter='%m/%d/%Y %I:%M:%S %p'
+_handlers = [{'handler':'StreamHandler','format': None,'datefmt': None,'args':{'stream':None}}, 
+             {'handler':'FileHandler','format': None,'datefmt': None,'args':{'filename':'log.log'}}
+            ]
+class Logger(object):
+    """
+    Logger level value, setting level all above levels make a log
+    CRITICAL    50
+    ERROR       40
+    WARNING     30
+    INFO        20  <--- Default level
+    DEBUG       10
+    NOTSET       0
+    """
+    
+    staticLogger = None
+    loggerLock = threading.Lock()
+    
+    def __init__(self, caller='Object', level=None, path=None):
+        self.level = logging.INFO if level is None else level
+        self.caller = caller  
+        if Logger.staticLogger is None:
+            self.logger = logging.getLogger('Logger')
+            self.logger.setLevel(self.level)
+            for han_opt in _handlers:
+                self.addHandler(han_opt,path)
+            Logger.staticLogger = self.logger
+        else:
+            self.logger = Logger.staticLogger 
+        
+    def addHandler(self,handler_opt=None,path=None):
+        if handler_opt is not None:
+            logModule = sys.modules['logging']
+            fn  = getattr(logModule,handler_opt['handler'])
+            if handler_opt['handler'] == 'FileHandler' and path is not None:
+                new_handler = fn(filename=path)
+            else:
+                new_handler = fn(**handler_opt['args'])
+            new_handler.setLevel(self.level)
+            fmt = _formatter if handler_opt['format'] is None else handler_opt['format']
+            datefmt = _dateformatter if handler_opt['datefmt'] is None else handler_opt['datefmt']
+            formatter = logging.Formatter(fmt=fmt,datefmt=datefmt)
+            new_handler.setFormatter(formatter)
+            self.logger.addHandler(new_handler)
+    
+    def info(self,msg):
+        Logger.loggerLock.acquire()
+        self.logger.info(msg,extra={'caller':self.caller})
+        Logger.loggerLock.release()
+    
+    def warn(self,msg):
+        Logger.loggerLock.acquire()
+        self.logger.warn(msg,extra={'caller':self.caller})
+        Logger.loggerLock.release()
+    
+    def debug(self,msg):
+        Logger.loggerLock.acquire()
+        self.logger.debug(msg,extra={'caller':self.caller})
+        Logger.loggerLock.release()
+        
+    def error(self,msg):
+        Logger.loggerLock.acquire()
+        self.logger.error(msg,extra={'caller':self.caller})
+        Logger.loggerLock.release()
+        
+    def critical(self,msg):
+        Logger.loggerLock.acquire()
+        self.logger.critical(msg,extra={'caller':self.caller})
+        Logger.loggerLock.release()
